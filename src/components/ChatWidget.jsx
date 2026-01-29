91a77637b99bd1c5878b574c60edeb915999ce83
MoisisV <MoisisV@gmail.com>
2026-01-29 16:05:58 +0200

    fix config

--- src/components/ChatWidget.jsx
+++ src/components/ChatWidget.jsx
@@ -35,7 +35,8 @@ const ChatWidget = () => {
                 icon: 'close',
                 onClick: () => setIsOpen(false),
             },
-        }, startScreen: {
+        },
+        startScreen: {
             greeting: "Hi! Ask me something 😊"
         },
     });
